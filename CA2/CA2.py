11ba1c031c363237c31c079589b18b85a4f1e771
PadraicM71 <padraic.moran@icloud.com>
2020-12-20 14:23:35 +0000

    Clean Up 1

--- CA2/CA2.py
+++ CA2/CA2.py
@@ -25,7 +25,7 @@ from os import system, name
 
 
 
-# Define our clear function 
+# clear screen when requested
 def clear(): 
     # for windows 
     if name == 'nt': 
@@ -43,7 +43,8 @@ current_game = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
 
 
-def display_board(board): # Board user interface function
+# prints tic tac toe board and current game positions for x or o
+def display_board(board):
     '''
     Tic Tac Toe Game Board
     User Interface
@@ -64,7 +65,9 @@ def display_board(board): # Board user interface function
     print('   |   |')
 
 
-
+# player move if player going first
+# take input position from player
+# validate input and execute move
 def player1Move():
     '''
     Accept player 1 input and validate it
@@ -72,14 +75,14 @@ def player1Move():
     Executes player 1 move if space is free.
     '''
     p1 = True
-    while p1 == True:
+    while p1 == True: # loop until input validated
         moveP1 = input ("Please choose a position for x Player 1: ")
         try:
             moveP1 = int(moveP1)
             if moveP1 in range (1,10):
-                if spaceFree(moveP1):
+                if spaceFree(moveP1): # check if space is free
                     p1 = False
-                    insertMove(moveP1, 'x')
+                    insertMove(moveP1, 'x') # if space free insert move
                 else:
                     print("This position is taken already!")
             else:
@@ -87,10 +90,12 @@ def player1Move():
 
         except:
             print("Please provide a number Player1!!")
-           # print(current_game) # debugging
+           
 
 
-
+# player move if player going second
+# take input position from player
+# validate input and execute move
 def player2Move():
     '''
     Accept player 2 input and validate it
@@ -98,14 +103,14 @@ def player2Move():
     Executes player 2 move if space is free.
     '''
     p2 = True
-    while p2 == True:
+    while p2 == True: # loop until input validated
         moveP2 = input ("Please choose a position for o Player 2: ")
         try:
             moveP2 = int(moveP2)
             if moveP2 in range (1,10):
-                if spaceFree(moveP2):
+                if spaceFree(moveP2): # check if space is free
                     p2 = False
-                    insertMove(moveP2, 'o')
+                    insertMove(moveP2, 'o') # if space free insert move
                 else:
                     print("This position is taken already!")
             else:
@@ -113,7 +118,7 @@ def player2Move():
 
         except:
             print("Please provide a number player2!!")
-           # print(current_game) # debugging
+        
 
 
 
@@ -127,15 +132,12 @@ def aiMove(aiGoFirstSecond, counter): # algorithm - computer move - can go first
 
     availableMoves = [number for number,item in enumerate(current_game) if item == ' ']
     availableMoves.pop(0)  
-    #print(availableMoves) # list of available moves
 
     #make a copy of current game board to evaluate its status and not change values of current_game
     boardCopy = []
     for i in current_game:
         boardCopy.append(i)
-    #print (current_game)
-    #print (boardCopy)
-    print ("counter" + str(counter))
+
     #check for a possible win in the next move and take it
     for t in availableMoves:
         boardCopy[t] = assignXO
@@ -161,20 +163,23 @@ def aiMove(aiGoFirstSecond, counter): # algorithm - computer move - can go first
     for q in availableMoves:
         if q in [1,3,7,9]:
             cornersOpen.append(q)
-    print(cornersOpen.count('x'))
+
+    #however if human chooses a corner first take 5
     if counter < 2 and len(cornersOpen) < 4: # if human player chooses a corner on first game move
         insertMove(5, assignXO)                 #algorithm takes center
         return
-    #my ai extra
+
+    #my ai extra however if human takes corner 1st and second move force a distraction
     checkcountXcorner = []
     checkcountXcorner.append(current_game[1])
     checkcountXcorner.append(current_game[3])
     checkcountXcorner.append(current_game[7])
     checkcountXcorner.append(current_game[9])
-    print(checkcountXcorner)
     if counter == 2 and checkcountXcorner.count('x') == 2 and aiGoFirstSecond == 1: # play position 4 if move count < 4 where player 1 picks 2 corners in row as first moves
         insertMove(4, assignXO)                                 #to block a double corner and player 1 goes first
         return
+
+    #now take a corner if above 2 conditions are not satisified
     if len(cornersOpen) > 0: #if more than 1 corner available choose a random one
         mC = random.choice(cornersOpen)
         insertMove(mC, assignXO)
@@ -190,8 +195,13 @@ def aiMove(aiGoFirstSecond, counter): # algorithm - computer move - can go first
     insertMove(last, assignXO)
 
 
-
+# basic random ai used for testing during project
+# not used at present but will keep in case of
+# future changes 
 def aiMoveRand(aiGoFirstSecond): # Basic Random ai used for testing later
+    '''
+    Used for testing only - picks random position
+    '''
     if aiGoFirstSecond == 0: # 0 implies 1st and assigns x - 1 implies 2nd assigns o
         assignXO = 'x'
     else:
@@ -206,7 +216,7 @@ def aiMoveRand(aiGoFirstSecond): # Basic Random ai used for testing later
 
 def spaceFree(position):
     '''
-    Checks position on Tic Tac Toe
+    Checks if position on Tic Tac Toe
     game board is free
     '''
     return current_game[position] == ' '
@@ -243,25 +253,30 @@ def checkBoardFull(gameinprogress):
     '''
     Checks if game board is full
     '''
-    if gameinprogress.count(' ') > 1:
+    if gameinprogress.count(' ') > 1: # as index 0 not used
         return False
     else:
         return True
 
 
 
-# Execute functions in game - put in main()
+# execute functions for a game
 def main():
     clear()
 
+    #print instrustions for player
+    display_board([' ','1','2','3','4','5','6','7','8','9'])
     print("Welcome to Padraic's Tic Tac Toe")
+    print("When instructed, using the above gameboard as a reference,")
+    print("enter your desired position on gameboard from 1-9.")
 
+    #take player input to decide if they go first or second
     choice = 'FirstSecond'
     within_range = False
     while choice.isdigit() == False or within_range == False:
         choice = input("Would you like to go first (type 1) or second (type 2)? ")
         if choice.isdigit() == False:
-            print("Sorry that is not a digit!")
+            print("Sorry, this is not Scrabble! Please provide a position from 1-9.")
         if choice.isdigit():
             if int(choice) in range(1,3):
                 within_range = True
@@ -271,10 +286,12 @@ def main():
 
     display_board(current_game)
 
-    counter = 1 #starts a counter for game moves - used for algorithm to decide initial moves
-    while (checkBoardFull(current_game)) == False:
-        if checkWin(current_game, 'o') == False:
-            if choice == 1:
+    counter = 1 #starts a counter for game moves - used in algorithm to decide initial moves
+    while (checkBoardFull(current_game)) == False: #loop until game completion
+
+        #first move by player or computer - first check if other player made a winning move
+        if checkWin(current_game, 'o') == False: 
+            if choice == 1: #choice decided if player or computer went first
                 player1Move()
             else:
                 aiMove(0,counter) #here parameter (0) indicates computer is taking 1st move
@@ -284,7 +301,7 @@ def main():
         else:
             print("o has won this time!")
             break
-        
+        #second move - comments as above
         if checkWin(current_game, 'x') == False:
             if checkBoardFull(current_game) == False:
                 if choice == 2:
@@ -304,17 +321,9 @@ def main():
         checkWin(current_game, 'x') == False and
         checkWin(current_game, 'o') == False):
             print ("It's a draw!")
-        # All 3 conditions must be satisified
+        # ALL 3 conditions must be satisified
         # for a draw - true,false,false.
 
-
-#-------------------------------------------------
-    # if spaceFree(5) == True:
-    #     print("free space")
-    # else:
-    #     print("not free")
-
-    # print(list(enumerate(current_game)))
 
 
 main()
